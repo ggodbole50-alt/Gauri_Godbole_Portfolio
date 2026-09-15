22e81eec15a0b318085183f913103ddec1685219
ggodbole50-alt <ggodbole50@gmail.com>
2026-09-15 10:42:22 +0530

    changed config.js file

--- Gauri_Godbole_Portfolio_SFD/docusaurus.config.js
+++ Gauri_Godbole_Portfolio_SFD/docusaurus.config.js
@@ -10,7 +10,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'Gauri Godbole Portfolio',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
@@ -48,9 +48,6 @@ const config = {
         docs: {
           sidebarPath: './sidebars.js',
           // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
