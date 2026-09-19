f0aac4d609d9e544997a33ebf8ddbc44588f27e6
ggodbole50-alt <ggodbole50@gmail.com>
2026-09-19 10:33:12 +0530

    updated config file

--- Gauri_Godbole_Portfolio/docusaurus.config.js
+++ Gauri_Godbole_Portfolio/docusaurus.config.js
@@ -20,15 +20,15 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://Gauri_Godbole_Portfolio.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docs-as-code-training/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'ggodbole50-alt', // Usually your GitHub org/user name.
+  projectName: 'Gauri_Godbole_Portfolio', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
